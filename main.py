ffa8ec7441c94c815445ee55b8668032750481d0
Akshay <76613246+Akshess@users.noreply.github.com>
2021-12-06 00:25:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 github_user = input('Input Github User:') #to take user name
-url  = 'https://github.com/'+github_user #url + concatinating user name
+url  = 'https://github.com/'+github_user #concatinating url and user name
 r = requests.get(url) #url request
 soup = bs(r.content, 'html.parser')# .content gives content of page
 profile_image = soup.find('img',{'alt':'Avatar'})['src']
